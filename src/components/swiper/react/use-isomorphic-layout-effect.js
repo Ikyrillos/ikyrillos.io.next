8f5ebad44f8c00b8a834d1c62eac21d4619a7e1d
Ikyrillos <kyrillosmaherfekri@gmail.com>
2024-02-08 01:25:44 +0200

    fix js error

--- src/components/swiper/react/use-isomorphic-layout-effect.js
+++ src/components/swiper/react/use-isomorphic-layout-effect.js
@@ -1,9 +1,10 @@
-import { useEffect, useLayoutEffect } from 'react';
+import { useEffect, useLayoutEffect } from "react";
 
 function useIsomorphicLayoutEffect(callback, deps) {
   // eslint-disable-next-line
-  if (typeof window === 'undefined') return useEffect(callback, deps);
-  return useLayoutEffect(callback, deps);
+  if (typeof window === "undefined") return useEffect(callback, deps);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  return useLayoutEffect(callback, [callback]);
 }
 
 export { useIsomorphicLayoutEffect };
